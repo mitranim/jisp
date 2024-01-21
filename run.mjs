2bd4516bc2ed0749702858427cbec0be321ed6a2
Nelo Mitranim <me@mitranim.com>
2024-01-21 19:10:01 +0100

    fix sample run script

--- run.mjs
+++ run.mjs
@@ -5,7 +5,7 @@ import * as d from './js/deno.mjs'
 Error.stackTraceLimit = 1024
 
 c.ctxGlobal[c.symFs] = new d.DenoFs()
-c.ctxGlobal[c.symTar] = new URL(`../.tmp_mock/`, import.meta.url).href
+c.ctxGlobal[c.symTar] = new URL(`./.tmp_mock/`, import.meta.url).href
 c.ctxGlobal.use = p.use
 
 const mod = await c.ctxReqModules(c.ctxGlobal).getInit(new URL(`main.jisp`, import.meta.url).href)
